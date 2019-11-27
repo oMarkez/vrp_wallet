2f78928fbc7773e01942f29c3dbbe6061cded621
oMarkez <misha66666699@gmail.com>
2019-11-27 16:31:24 -0500

    fix

--- vrp_wallet/server.lua
+++ vrp_wallet/server.lua
@@ -4,20 +4,14 @@ local Proxy = module("vrp", "lib/Proxy")
 vRP = Proxy.getInterface("vRP")
 vRPclient = Tunnel.getInterface("vRP", "vrp_wallet")
 
-local user_id = vRP.getUserId({source})
-local money = vRP.getMoney({user_id})
-local bank = vRP.getBankMoney({user_id})
-
 RegisterServerEvent('vrp_wallet:getMoneys')
 AddEventHandler('vrp_wallet:getMoneys', function()
+	local user_id = vRP.getUserId({source})
 
-	local _source = source
+	if user_id then
+		local money = vRP.getMoney({user_id})
+		local bank = vRP.getBankMoney({user_id})
 
-	if user_id == nil then
-		local money = vRP.getMoney({source})
-		local bank 	= vRP.getBankMoney({source})
-
-	    TriggerClientEvent("vrp_wallet:setValues", source, money, bank)
+	    	TriggerClientEvent("vrp_wallet:setValues", source, money, bank)
 	end
-
 end)
